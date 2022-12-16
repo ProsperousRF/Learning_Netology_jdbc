4bcbbc7c7776ee4ebb70b5468ff8676cfd31e62f
Stanislav Rakitov <prosperousrf@gmail.com>
2022-12-16 20:44:53 +0300

    Initial commit

---
+++ src/main/java/com/rakitov/netology/jdbc/LearningNetologyJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.rakitov.netology.jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningNetologyJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningNetologyJdbcApplication.class, args);
+    }
+
+}
