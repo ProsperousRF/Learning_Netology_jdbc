2abbf9cbac94aba0ad61f2f54465df83456abc70
Stanislav Rakitov <prosperousrf@gmail.com>
2022-12-21 14:50:59 +0300

    get products by user name sql

--- src/main/resources/product_by_name.sql
+++ src/main/resources/product_by_name.sql
@@ -1,5 +1,3 @@
-USE netology_test;
-
 SELECT product_name FROM ORDERS O
-    JOIN CUSTOMERS C ON C.id = O.customer_id
-    WHERE lower(C.name) = lower(:name);
+JOIN CUSTOMERS C ON C.id = O.customer_id
+WHERE lower(C.name) = lower(:name);
